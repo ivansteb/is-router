dad744cadf14b0a758ebb0796d87fe968f2c771f
= <=>
2024-06-12 15:57:07 -0300

    Lazy load

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
+import { Suspense, lazy } from 'react';
+
 import './App.css';
-import HomePage from './pages/Home';
-import AboutPage from './pages/About';
 import Page404 from './pages/404';
 import SearchPage from './pages/Search';
 
 import { Router } from './Router';
 import { Route } from './Route';
+
+const LazyHomePage = lazy(() => import('./pages/Home.jsx'))
+const LazyAboutPage = lazy(() => import('./pages/About.jsx'))
+
 
 const appRoutes = [
   {
@@ -17,10 +21,12 @@ const appRoutes = [
 function App () {
   return (
     <main>
-      <Router routes={appRoutes} defaultComponent={Page404}>
-        <Route path='/' Component={HomePage} />
-        <Route path='/about' Component={AboutPage} />
-      </Router>
+      <Suspense fallback={null}>
+        <Router routes={appRoutes} defaultComponent={Page404}>
+          <Route path='/' Component={LazyHomePage} />
+          <Route path='/about' Component={LazyAboutPage} />
+        </Router>
+      </Suspense>
     </main>
   )
 }
